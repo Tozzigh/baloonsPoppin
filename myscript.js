661c8f57e29f325df41fbbc6fd0e307546d08638
Tozzigh <tozzigh@gmail.com>
2020-04-26 11:09:09 +0000

    Ni tan mal

--- myscript.js
+++ myscript.js
@@ -1,7 +1,5 @@
-var cnt=0;
-function CountFun(){
-    cnt=parseInt(cnt)+parseInt(1);
-    var divData=document.getElementById("showCount");
-    divData.innerHTML="Number of Downloads: ("+cnt +")";
+var count=0;
+function countFun(){
+    count+=1;
+    document.getElementById("showCount").innerHTML=count +" popped";
 }
-CountFun();
