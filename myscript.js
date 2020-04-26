86d25c24cb8bdbc9e173d8929601fb998164d210
Tozzigh <tozzigh@gmail.com>
2020-04-26 15:42:34 +0000

    Fixed position

--- myscript.js
+++ myscript.js
@@ -1,5 +1,8 @@
-var count=0;
+var count=15;
 function countFun(){
-    count+=1;
-    document.getElementById("showCount").innerHTML=count +" popped";
+    count-=1;
+    document.getElementById("showCount").innerHTML=count +" remaining";
+    
 }
+var resetBtn = document.getElementById("reset");
+    resetBtn.disabled = "disabled"
